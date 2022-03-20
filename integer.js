b1bedd62c130d68a54a639f6b25bcd35c1e9f3e1
laziza@mail.ru <laziza00>
2022-03-20 15:11:59 +0500

    initial commit

--- integer.js
+++ integer.js
@@ -1,22 +1,4 @@
-// let arr2 = [8,9,10,8,10,11,11, 1];
 
-// let b=[]
-// let count = arr2.length
-
-// for (let i=0; i<count; i++) {
-//     let k =[]
-//     let f = [];
-//     for (let j =0; j<arr2.length; j++) {
-//         if (arr2[0] == arr2[j]) {
-//             k.push (arr2[j])
-//         }else {
-//             f.push(arr2[j])
-//         }
-//     }
-//     arr2 = f;
-//     if (k !="")b.push(k)
-
-// }console.log(b);
 
 
 
