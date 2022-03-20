754d5e971168a59d0d6f4b5ea9b75dffc6b289b9
laziza@mail.ru <laziza00>
2022-03-20 15:05:33 +0500

    initial commit

--- integer.js
+++ integer.js
@@ -21,6 +21,87 @@
 
 
 
+let list = document.querySelector('#list')
+let addBtn = document.querySelector('#addbtn1');
+let list2 = document.querySelector('#listcart');
+let subtotal = document.querySelector('#subtotal');
+let tax = document.querySelector('#tax');
+let total =document.querySelector('#total');
+let removeBtn = document.querySelectorAll('.pizza__remove-btn')
+
+
+let arr = [
+    {
+    name: "papperoni",
+    price: 5,
+    id : 1,
+    imgurl: "https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg "
+},
+     {
+    name: "cheesy",
+    price: 10,
+    id : 2,
+    imgurl: 'https://quizzical-murdock-fa5953.netlify.app/img/pizza1.png'
+
+},
+    {
+    name: "kombo",
+    price: 20,
+    id : 3,
+    imgurl: "https://quizzical-murdock-fa5953.netlify.app/img/pizza3.jpg "
+
+},
+{
+    name: "margarita",
+    price: 30,
+    id : 4,
+    imgurl: "https://quizzical-murdock-fa5953.netlify.app/img/pizza2.jpg"
+
+}
+];
+
+
+for (let i=0; i<arr.length; i++) {
+
+    let li = document.createElement('li');
+    li.className = "pizza__item"
+    li.innerHTML = `
+
+    <div class="pizza__img-box">
+    <img class='pizza__img' src =" ${arr[i].imgurl} " alt= "pizza"/>
+    </div>
+    <div class="pizza__item-box">
+    <h3 class = 'pizza__item-title'> ${arr[i].name}</h2>
+    <p class="pizza__item-price">$ ${arr[i].price}</p>
+    <button class="pizza__btn" onclick = "getAdd(${arr[i].id})">Add to Cart </button>
+    </div>
+ `;
+
+ list.appendChild(li);
+}
+
+
+
+
+let newArr = [];
+
+
+
+function addItem(id) {
+
+    for (let i = 0; i < arr.length; i++) {
+   
+        if (id == arr[i].id) {
+
+            console.log(arr[i]);    
+           return arr[i];
+      
+        }
+    }
+}
+
+
+
 
 function checkPizzaItem (item) {
     if(!newArr.length) {
@@ -59,7 +140,7 @@ function removeItem (id) {
         }
     }
     newArr = arrBox;
-    sortItem ();
+    sortItem();
 }
 
 
@@ -71,21 +152,21 @@ function sortItem () {
     for (let i=0; i<newArr.length; i++){
         let li = document.createElement('li');
 
-        li.className = "pizza__item cart__item"
+        li.className = "cart__item"
         li.innerHTML = `
                 <div class="pizza__img-box">
                 <img class="pizza__img" src="${newArr[i].imgurl}" alt="img">
                 </div>
                 <div class="pizza__item-box">
                     <div>  
-                            <h3 class="pizza__item-title">${newArr[i].name}</h3>
-                            <p class="pizza__item-price">$${newArr[i].price *newArr[i].count}</p>
+                        <h3 class="pizza__item-title">${newArr[i].name}</h3>
+                        <p class="pizza__item-price">$${newArr[i].price *newArr[i].count}</p>
                      </div>
                     <button  class="pizza__remove-btn count__btn" >${newArr[i].count}</button>
 
                     <div class="pizza__cart-btns" style =""> 
-                    <button  class=" pizza__remove-btn" onclick='addItem(${i})'>+</button>
-                    <button  class="pizza__remove-btn " onclick='removeItem(${i})'>-</button>
+                    <button  class=" pizza__remove-btn" onclick="addButton(${i})">+</button>
+                    <button  class="pizza__remove-btn " onclick="removeButton(${i})">-</button>
                     </div>
                     
                 </div>
@@ -96,24 +177,25 @@ function sortItem () {
 
     }
 
-    let tax1 = subtitle *0.1;
-    let total1 = tax1 + subtitle;
-
-    subtotal.innerHTML=`${subtitle}$`;
-    tax.innerHTML=`${tax1}$`;
-    total.innerHTML=`${total1}$`
-
 }
 
 
+let tax1 = subtitle *0.1;
+let total1 = tax1 + subtitle;
 
-function addItem(index) {
-    newArr[index].count = newArr[index].count +1
+subtotal.innerHTML=`${subtitle}$`;
+tax.innerHTML=`${tax1}$`;
+total.innerHTML=`${total1}$`
+
+
+function addButton(index) {
+    newArr[index].count = newArr[index].count +1;
 
     sortItem()
 }
 
-function removeItem(index) {
+function removeButton(index) {
+
     let pizzaIndex = newArr[index];
     
     if (pizzaIndex.count >1){
